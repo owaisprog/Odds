9922cead3e3984a855cbb820786310ffcb58903b
Sanan Ahmad <sananahmad909@gmail.com>
2025-11-04 13:21:01 +0500

    vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
-
-  // Add this block to force Next to include Prisma client + engines in serverless bundles
-  experimental: {
-    outputFileTracingIncludes: {
-      // App Router route handlers
-      "app/**": [
-        "./node_modules/@prisma/client",
-        "./node_modules/.prisma/client/**/*",
-      ],
-      // If you also use Pages API routes, keep this; otherwise you can remove it
-      "pages/api/**": [
-        "./node_modules/@prisma/client",
-        "./node_modules/.prisma/client/**/*",
-      ],
-    },
+    remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
   },
 };
 
